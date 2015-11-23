f2376cb702d4ecdd66f79ff74efcc3ce27c8b41a
caiya928 <caiya928@aliyun.com>
2015-11-23 17:12:10 +0800

    提交

---
+++ app/modules/dao.go
@@ -0,0 +1,29 @@
+package modules
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	DbName         = "blog"
+	UserConnection = "user"
+	BlogConnection = "blog"
+)
+
+type Dao struct {
+	session *mgo.Session
+}
+
+func Conn() (*Dao, err) {
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		return nil, err
+	}
+	return &Dao{session}, nil
+}
+
+func (dao *Dao) Close() {
+	dao.session.Close()
+}
